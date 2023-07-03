f7000069605fb3d14a152b559e50c449b8bc7c32
vengerz <natthawut.gew@gmail.com>
2023-07-03 16:18:30 +0700

    modified:   as2grade.js 	modified:   as2grade.ts

--- as2grade.js
+++ as2grade.js
@@ -5,24 +5,25 @@ for (var x in score) {
     var sc2 = +score[x];
     sum = sum + sc2;
 }
-console.log("\u0E1C\u0E25\u0E23\u0E27\u0E21\u0E04\u0E30\u0E41\u0E19\u0E19: ".concat(sum));
+var gradesum = sum;
 var msg1;
-if (sum >= 80 && sum <= 100) {
+if (gradesum >= 80 && gradesum <= 100) {
     msg1 = "Grade: A";
 }
-else if (sum >= 70) {
+else if (gradesum >= 70 && gradesum <= 79) {
     msg1 = "Grade: B";
 }
-else if (sum >= 60) {
+else if (gradesum >= 60 && gradesum <= 69) {
     msg1 = "Grade: C";
 }
-else if (sum >= 50) {
+else if (gradesum >= 50 && gradesum <= 59) {
     msg1 = "Grade: D";
 }
-else if (sum < 49 && sum >= 0) {
+else if (gradesum <= 49 && sum >= 0) {
     msg1 = "Grade: F";
 }
 else {
     msg1 = "ลองใหม่อีกครั้ง";
 }
+console.log("\u0E1C\u0E25\u0E23\u0E27\u0E21\u0E04\u0E30\u0E41\u0E19\u0E19: ".concat(gradesum));
 console.log(msg1);
--- as2grade.ts
+++ as2grade.ts
@@ -5,19 +5,21 @@ for (let x in score){
     let sc2:number = +score[x]
     sum = sum + sc2
 }
-console.log(`ผลรวมคะแนน: ${sum}`)
+
+let gradesum:number = sum 
 let msg1:string;
-if (sum>=80&&sum<=100){
+if (gradesum>=80&&gradesum<=100){
     msg1 = "Grade: A"
-}else if (sum>=70){
+}else if (gradesum>=70&&gradesum<=79){
     msg1 = "Grade: B"
-}else if (sum>=60){
+}else if (gradesum>=60&&gradesum<=69){
     msg1 = "Grade: C"
-}else if (sum>=50){
+}else if (gradesum>=50&&gradesum<=59){
     msg1 = "Grade: D"
-}else if (sum<49&&sum>=0){
+}else if (gradesum<=49&&sum>=0){
     msg1 = "Grade: F"
 }else{
     msg1 = "ลองใหม่อีกครั้ง"
 }
+console.log(`ผลรวมคะแนน: ${gradesum}`)
 console.log(msg1)
